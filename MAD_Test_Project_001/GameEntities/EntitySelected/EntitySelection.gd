dd4f935568274016aea3bac99dfec1743a960098
Daniel Jones <daniel.lachlan.jones@gmail.com>
2021-07-05 20:43:44 +1000

    Added a EntitySelection Scene which works properly now.

--- MAD_Test_Project_001/GameEntities/EntitySelected/EntitySelection.gd
+++ MAD_Test_Project_001/GameEntities/EntitySelected/EntitySelection.gd
@@ -1,13 +1,25 @@
+# 
+# Informaiton about having editor change dynamically
+# https://docs.godotengine.org/en/stable/tutorials/misc/running_code_in_the_editor.html#doc-running-code-in-the-editor
+#
+tool
 extends Node
 
-# Need to define this below signal 
+export(ImageTexture) var SelectionSprite setget setSelectionSprite
+export(Vector2) var SelectionArea = Vector2(1, 1) setget setSelectionArea
+
+var SelectionImageSize : Vector2 = Vector2(100, 100)
+var DefaultSelectionSize : Vector2 = Vector2(1, 1) * 2
+var Scale : Vector2 = DefaultSelectionSize / SelectionImageSize
+
 
 var Selected : bool = false
 var PlayerCountry : bool = false
 
-func _ready():
-	$SelectedSprite.visible = false
-	
+func _ready():	
+	if not Engine.editor_hint:
+		$SelectedSprite.visible = false
+			
 func setSelected(selected: bool) -> void:
 	$SelectedSprite.visible = selected
 
@@ -19,3 +31,21 @@ func _on_Selection_input_event(viewport, event, shape_idx):
 	
 	if event.button_index == BUTTON_LEFT:
 		 Signals.emit_signal("EntitySelected", self)
+		
+func setSelectionSprite(texture):	
+	SelectionSprite = texture
+	if not is_inside_tree():
+		yield(self, 'ready');
+		
+	$SelectedSprite.texture = SelectionSprite
+			
+func setSelectionArea(selectionArea: Vector2):	
+	SelectionArea = selectionArea
+	
+	#fix the scale of the selection sprite
+	var Scale = (SelectionArea / SelectionImageSize) * 2
+	$SelectedSprite.set_scale(Scale)
+		
+	$SelectionArea.shape.set_extents(SelectionArea)
+	
+	
