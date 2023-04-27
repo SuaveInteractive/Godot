714228cbe1a37ed55abd0a15d6ce6e851254ad81
Daniel Jones <daniel.lachlan.jones@gmail.com>
2023-04-27 22:04:58 +1000

    The location of Debug Windows is now saved between runs.  Added an ignore to the addons folder to stop the files showing up in searches.

--- MAD_Test_Project_001/Debug/DebugWindow/ControlWindow.gd
+++ MAD_Test_Project_001/Debug/DebugWindow/ControlWindow.gd
@@ -2,10 +2,18 @@ extends Control
 
 signal WindowClosed()
 
+export var SavePosition : bool = true
+export (Resource) var Settings
+
 var drag_position = null
+var resourcePath : String = "user://DebugPersistentData.tres"
 
 func _ready():
-	pass
+	if ResourceLoader.exists(resourcePath):
+		Settings = ResourceLoader.load(resourcePath)
+	else:
+		Settings = load("res://Debug/DebugWindow/DebugWindowSettings.gd").new()
+	rect_global_position = Settings.SavedPos
 
 func setWindowName(var name : String) -> void:
 	$TitleBar/WindowNameLabel.text = name
@@ -18,9 +26,14 @@ func _on_Window_gui_input(event):
 			drag_position = null
 	if event is InputEventMouseMotion and drag_position:
 		rect_global_position = get_global_mouse_position() - drag_position
-
-
+		
+	Settings.SavedPos = rect_global_position
+	
 func _on_Button_pressed():
+	var err = ResourceSaver.save(resourcePath, Settings)
+	if err:
+		var stringError : String = "[DebugControlWindow Scene]: Error occured trying to save Persistent Debug Data.  Error [" + err + "]"
+		push_error(stringError)
 	hide()
 	queue_free()
 	emit_signal("WindowClosed")
---
+++ MAD_Test_Project_001/Debug/DebugWindow/DebugWindowSettings.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name DebugWindowSettings
+
+export var SavedPos : Vector2 = Vector2(0,0)
